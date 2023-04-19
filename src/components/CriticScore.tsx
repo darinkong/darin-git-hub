15ac743dcfa8023df4029a9afb1d41bd52dbaf64
Darin Kong <xkongdarin@gmail.com>
2023-04-20 01:17:54 +0800

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  return (
+    <>
+      <Badge
+        fontSize="14px"
+        paddingX={2}
+        borderRadius="4px"
+        colorScheme={score >= 90 ? "green" : "yellow"}
+      >
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
